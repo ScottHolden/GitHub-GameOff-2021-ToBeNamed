c6d0889fd0b27abff46e918a5645f4da116e314c
Scott Holden <scott@nullops.io>
2021-11-02 17:39:10 +1100

    Added publish

--- BugTesting/wwwroot/game.js
+++ BugTesting/wwwroot/game.js
@@ -21,6 +21,6 @@
 		renderViaOffscreenBackBuffer: 0,
 	};
 	const gl = canvas[0].getContext("webgl2", contextAttributes);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 }
