016b41b1c2c18c61c2d1bffbca5b43f6c0c32322
Scott Holden <scott@nullops.io>
2021-11-02 17:29:18 +1100

    Base razor & js

---
+++ BugTesting/Game.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BugTesting
+{
+    public partial class Game
+    {
+        private ElementReference htmlCanvas;
+
+        [Inject]
+        IJSRuntime JS { get; set; } = null!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender) return;
+
+            var module = await JS.InvokeAsync<IJSUnmarshalledObjectReference>("import", "./game.js");
+
+            await module.InvokeVoidAsync("init", new [] { htmlCanvas });
+        }
+    }
+}
---
+++ BugTesting/wwwroot/game.js
@@ -0,0 +1,29 @@
+﻿export function init(canvas) {
+	const resize = () => {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	};
+	window.addEventListener('resize', resize);
+	resize();
+	const contextAttributes = {
+		alpha: 1,
+		depth: 1,
+		stencil: 8,
+		antialias: 1,
+		premultipliedAlpha: 1,
+		preserveDrawingBuffer: 0,
+		preferLowPowerToHighPerformance: 0,
+		failIfMajorPerformanceCaveat: 0,
+		majorVersion: 2,
+		minorVersion: 0,
+		enableExtensionsByDefault: 1,
+		explicitSwapControl: 0,
+		renderViaOffscreenBackBuffer: 0,
+	};
+	const gl = canvas[0].getContext("webgl2", contextAttributes);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	window.gl = gl;
+	console.log(gl);
+	crash;
+}
